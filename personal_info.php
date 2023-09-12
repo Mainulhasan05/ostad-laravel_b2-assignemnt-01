2f1b0a07e422ed78c58d9878c9c5c813c922eb8c
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2023-09-12 14:21:31 +0600

    task 1 completed

---
+++ personal_info.php
@@ -0,0 +1,11 @@
+<?php
+$name="Md. Mainul Hasan";
+$age=25;
+$country="Bangladesh";
+$introduction="I am a web developer. I am from Bangladesh. I am 25 years old.";
+// echo them all beautiful structure
+echo "Name: ".$name."\n";
+echo "Age: ".$age."\n";
+echo "Country: ".$country."\n";
+echo "Introduction: ".$introduction."\n";
+?>
