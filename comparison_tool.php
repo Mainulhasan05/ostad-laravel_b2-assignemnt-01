31248758f6ebade3529c4c0cad57510cd49f2d24
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2023-09-13 10:07:21 +0600

    task 6 completed

---
+++ comparison_tool.php
@@ -0,0 +1,8 @@
+<?php
+$num1=65;
+$num2=20;
+echo $num1>$num2?$num1." is greater than ".$num2."\n":$num2." is greater than ".$num1."\n";
+echo $num1<$num2?$num1." is less than ".$num2."\n":$num2." is less than ".$num1."\n";
+echo $num1==$num2?$num1." is equal to ".$num2."\n":$num2." is equal to ".$num1."\n";
+
+?>
